0dcb16f2aeb77078446fe54fabf8a51f62f74542
Juan Felipe Arias <juanfeariascastillo@live.com>
2023-08-07 22:44:03 -0500

    Delete boleteria directory

--- boleteria/src/main/java/com/example/boleteria/interfaces/EventoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.boleteria.interfaces;
-
-import com.example.boleteria.model.Evento;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EventoRepository extends JpaRepository<Evento, Long> {
-}
--- boleteria/src/main/java/com/example/boleteria/interfaces/SalaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.boleteria.interfaces;
-
-import com.example.boleteria.model.Sala;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SalaRepository extends JpaRepository<Sala, Long> {
-}
